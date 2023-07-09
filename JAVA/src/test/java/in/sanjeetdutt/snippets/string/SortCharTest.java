e41031a6012a9a22540b3357975a779e55b0eadc
Sanjeet Dutt <sdutt@peoplestrategy.com>
2023-07-09 10:36:09 +0530

    String : Sort Chars

---
+++ JAVA/src/test/java/in/sanjeetdutt/snippets/string/SortCharTest.java
@@ -0,0 +1,16 @@
+package in.sanjeetdutt.snippets.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortCharTest {
+
+    @Test
+    void sort() {
+
+        SortChar sortChar = new SortChar();
+
+        assertEquals(sortChar.sort("asdfgs"),"adfgss");
+    }
+}
